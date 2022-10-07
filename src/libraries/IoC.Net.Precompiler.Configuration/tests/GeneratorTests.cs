681e36c23693c601d0d3b712ce92d9ce040d5dd9
Eugene Sadovoi <evgeni@eniks.com>
2022-10-07 15:08:21 -0700

    Adding composition library

---
+++ src/libraries/IoC.Net.Precompiler.Configuration/tests/GeneratorTests.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Reflection;
+using Xunit;
+
+namespace Configuration.Tests
+{
+    public class ConfigurationTests
+    {
+        [Fact]
+        public void SimpleGeneratorTest()
+        {
+        }
+
+    }
+}
